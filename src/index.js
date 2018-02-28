e85a2006450d46ad4397096273c6c50285196e48
Pavel <pahonski@gmail.com>
2018-02-28 10:13:00 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function getZerosCount(number) {
   // your implementation
-}
+    let counter = 5;
+    let zeros = 0;
+
+    function getFactorialZeros(counter) {
+        if (number / counter >= 1) {
+            zeros += Math.floor(number / counter);
+            counter *= 5;
+            getFactorialZeros(counter);
+        } else {
+            return;
+        }
+    }
+
+    getFactorialZeros(counter);
+
+    return zeros;
+
+};
